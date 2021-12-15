311749a39e8add4cfe23d6ace60cb4d177794d56
Lesnicho <lesnik.artemij@bk.ru>
2021-12-15 14:23:29 +0300

    Выполнели задачу повторно

---
+++ index.js
@@ -0,0 +1,8 @@
+let input = document.querySelector('.inpu1')
+let button = document.querySelector('.btn')
+
+let p = document.querySelector('.p')
+
+button.onclick = ()=>{
+    p.innerHTML = input.value.length    
+}
